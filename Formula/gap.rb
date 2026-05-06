d7c0cb01f1e9d1eedcaabd0d45946bfdb08a8615
Dima Pasechnik <dima@pasechnik.info>
2026-05-05 22:15:37 -0700

    satisfy Homebrew test Action

--- Formula/gap.rb
+++ Formula/gap.rb
@@ -5,25 +5,23 @@ class Gap < Formula
   sha256 "6049d53e99b12e25c2d848db21ac4a06380a46fe4c4157243d556fe06930042c"
   license "GPL-2.0-or-later"
 
-  depends_on "gmp"
-  # GAP cannot be built against the native macOS version of readline
-  # it requires either GNU readline, or no readline at all; but
-  # the latter leads to an inferior user experience.
-  # So we depend on GNU readline here.
-  depends_on "readline"
-
-  # for packages
+  # most dependencies are for for packages; only gmp and readline are for GAP itself
   depends_on "cddlib"   # CddInterface
   depends_on "curl"     # curlInterface
   depends_on "fplll"    # float
+  depends_on "gmp"      # - for main GAP
   depends_on "libmpc"   # float
   depends_on "mpfi"     # float
   depends_on "mpfr"     # float, normalizinterface
   depends_on "ncurses"  # browse
   depends_on "pari"     # alnuth
+  # GAP cannot be built against the native macOS version of readline
+  # it requires either GNU readline, or no readline at all; but
+  # the latter leads to an inferior user experience.
+  # So we depend on GNU readline here.
+  depends_on "readline" # - for main GAP
   depends_on "singular" # many packages
   depends_on "zeromq"   # ZeroMQInterface
-
 
   def install
     system "./configure", *std_configure_args
@@ -31,8 +29,9 @@ class Gap < Formula
 
     ohai "Building included packages. Please be patient, it may take a while"
 
-    system "mkdir", "-p", "#{lib}/gap/"
-    system "cp", "-R", "pkg", "#{lib}/gap/pkg"
+    require "fileutils"
+    mkdir_p "#{lib}/gap/"
+    cp_r "pkg", "#{lib}/gap/pkg"
 
     cd lib/"gap/pkg" do
       # NOTE: This script will build most of the packages that require
