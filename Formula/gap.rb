8c14784e318ecc2545093062febc09630e48c212
Dima Pasechnik <dima@pasechnik.info>
2026-07-02 18:44:04 -0500

    only require zlib on Linux

--- Formula/gap.rb
+++ Formula/gap.rb
@@ -32,7 +32,10 @@ class Gap < Formula
   depends_on "readline" # - for main GAP
   depends_on "singular" # many packages
   depends_on "zeromq"   # ZeroMQInterface
-  depends_on "zlib-ng-compat" # a 2nd order dep.
+
+  on_linux do
+    depends_on "zlib-ng-compat" # a 2nd order dep.
+  end
 
   def install
     system "./configure", *std_configure_args
